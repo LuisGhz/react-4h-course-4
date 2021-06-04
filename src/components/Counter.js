bb66aec0b8e2b0d8e202965938eeff2f0eb18c5d
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-04 13:37:30 -0500

    Import and use Button, ButtonGroup from reactstrap

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,15 @@
 import { useReducer } from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Counter = () => {
   return (
     <div>
-      
+      <ButtonGroup>
+        <Button color="dark" > Left </Button>
+        <Button color="dark" > Center </Button>
+        <Button color="danger" > Right </Button>
+      </ButtonGroup>
     </div>
   );
 }
