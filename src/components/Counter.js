8cd54cce5fcbe75e796de8e32f3262a537380cdb
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-04 13:34:42 -0500

    Create Counter component Import bootstrap styles. import userReducer

---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import { useReducer } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Counter = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Counter;
