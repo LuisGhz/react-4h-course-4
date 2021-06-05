4fbaa02d323292bb19f04bd1e1e020eecd86dba7
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-05 13:19:24 -0500

    Use useContext, reacstrap components. Import CounterContext Increment and Decrement

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,7 +1,23 @@
+import { useContext } from 'react';
+import { Button, ButtonGroup, Badge } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { CounterContext } from 'App';
+
 const ComponentA = () => {
+  const { counter, dispatch } = useContext(CounterContext);
   return (
     <div>
-
+      <ButtonGroup>
+        <Button color="primary" outline >
+          ComponentA Counter <Badge color="secondary">{ counter.counter }</Badge>
+        </Button>
+      </ButtonGroup>
+      <p></p>
+      <ButtonGroup>
+        <Button color="dark" onClick={ () => dispatch({type: 'increment'}) } > Increment 1 </Button>
+        <Button color="dark" onClick={ () => dispatch({type: 'decrement'}) } > Decrement 1 </Button>
+        <Button color="danger" onClick={ () => dispatch({type: 'reset'}) } > Reset </Button>
+      </ButtonGroup>
     </div>
   );
 }
