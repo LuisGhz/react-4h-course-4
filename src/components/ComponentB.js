04dfd9ddc8b424c13885c6ded8d2021d1f68cdbf
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-05 13:35:31 -0500

    Create Components B and C. Copy of ComponentA

---
+++ src/components/ComponentB.js
@@ -0,0 +1,25 @@
+import { useContext } from 'react';
+import { Button, ButtonGroup, Badge } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { CounterContext } from 'App';
+
+const ComponentB = () => {
+  const { counter, dispatch } = useContext(CounterContext);
+  return (
+    <div>
+      <ButtonGroup>
+        <Button color="primary" outline >
+          ComponentB Counter <Badge color="secondary">{ counter.counter }</Badge>
+        </Button>
+      </ButtonGroup>
+      <p></p>
+      <ButtonGroup>
+        <Button color="dark" onClick={ () => dispatch({type: 'increment'}) } > Increment 1 </Button>
+        <Button color="dark" onClick={ () => dispatch({type: 'decrement'}) } > Decrement 1 </Button>
+        <Button color="danger" onClick={ () => dispatch({type: 'reset'}) } > Reset </Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default ComponentB;
