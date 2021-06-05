3ff66459b52d9042b0027e6341003ff98611a5b8
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-05 13:01:42 -0500

    Remove Counter component. Move here initialState, reducer and useReducer from Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useReducer } from 'react';
 import './App.css';
-import Counter from 'components/Counter';
+
+const initialState = {
+  counter: 0
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, counter: state.counter + 1 };
+      case 'decrement':
+      return { ...state, counter: state.counter - 1 };
+    case 'reset':
+      return {...initialState};
+    default:
+      return state;
+  }
+}
 
 function App() {
+
+  const [status, dispatch] = useReducer(reducer, { ...initialState })
   return (
     <div className="App">
-      <Counter />
     </div>
   );
 }
